f9f3c9c738baf07a5544b9c87e54adccf0be8db6
William Blasko <williamblasko@gmail.com>
2014-10-20 17:19:52 -0400

    Revert Sean's notes since I cloned late.

--- chapter-2/weird-logger.js
+++
@@ -1,8 +0,0 @@
-function weirdLogger() {
-  return function(message) {
-    console.log('weird!', message);
-  }
-}
-var logger = weirdLogger();
-logger('is this thing on?');
-//=> weird! is this thing on?
